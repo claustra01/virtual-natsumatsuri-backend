6a5c608d717c595fea7ae76a50395159224d7553
claustra01 <shinya430.g@gmail.com>
2024-08-10 15:00:03 +0900

    add: parse websocket message

--- src/handler/shooter.rs
+++ src/handler/shooter.rs
@@ -1,8 +1,7 @@
 use std::net::SocketAddr;
 
 use axum::{
-    extract::connect_info::ConnectInfo,
-    extract::ws::{WebSocket, WebSocketUpgrade},
+    extract::{connect_info::ConnectInfo, ws::{Message, WebSocket, WebSocketUpgrade}},
     response::IntoResponse,
 };
 
@@ -16,18 +15,35 @@ pub async fn shooter_handler(
 
 async fn handle_socket(mut socket: WebSocket, addr: SocketAddr) {
     while let Some(msg) = socket.recv().await {
-        let msg = if let Ok(msg) = msg {
-            msg
+        if let Ok(msg) = msg {
+
+            let msg_txt = msg.to_text();
+            if let Ok(msg_txt) = msg_txt {
+                
+                let result = serde_json::from_str::<crate::model::shooter::ShooterData>(msg_txt);
+                if let Ok(data) = result {
+                    println!("{:?}", data.id);
+                    if socket.send(Message::Text(data.id)).await.is_err() {
+                        // client disconnected
+                        println!("{:?} disconnected.", addr);
+                        return;
+                    }
+
+                } else {
+                    println!("{:?} sent invalid JSON.", addr);
+                    return;
+                }
+
+            } else {
+                // client sent invalid UTF-8
+                println!("{:?} sent invalid UTF-8.", addr);
+                return;
+            }
+
         } else {
             // client disconnected
             println!("{:?} disconnected.", addr);
             return;
         };
-
-        if socket.send(msg).await.is_err() {
-            // client disconnected
-            println!("{:?} disconnected.", addr);
-            return;
-        }
     }
 }
