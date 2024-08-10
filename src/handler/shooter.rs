a48ed8d2042e23dbfcbdbaae2057c66a08504252
claustra01 <shinya430.g@gmail.com>
2024-08-10 14:29:50 +0900

    update: websocket handler

--- src/handler/shooter.rs
+++ src/handler/shooter.rs
@@ -1,5 +1,31 @@
-use axum::{http::StatusCode, response::IntoResponse};
+use std::net::SocketAddr;
 
-pub async fn shooter_handler() -> impl IntoResponse {
-    (StatusCode::OK, "Shooter Handler")
+use axum::{
+    extract::connect_info::ConnectInfo,
+    extract::ws::{WebSocket, WebSocketUpgrade},
+    response::IntoResponse,
+};
+
+pub async fn shooter_handler(
+    ws: WebSocketUpgrade,
+    ConnectInfo(addr): ConnectInfo<SocketAddr>,
+) -> impl IntoResponse {
+    println!("{:?} connected.", addr);
+    ws.on_upgrade(move |socket| handle_socket(socket, addr))
+}
+
+async fn handle_socket(mut socket: WebSocket, addr: SocketAddr) {
+    while let Some(msg) = socket.recv().await {
+        let msg = if let Ok(msg) = msg {
+            msg
+        } else {
+            // client disconnected
+            return;
+        };
+
+        if socket.send(msg).await.is_err() {
+            // client disconnected
+            return;
+        }
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::net::SocketAddr;
+
 mod handler;
 mod model;
 mod router;
@@ -7,5 +9,10 @@ async fn main() {
     let app = router::create_router();
     let listener = tokio::net::TcpListener::bind("0.0.0.0:8080").await.unwrap();
     println!("listening on {}", listener.local_addr().unwrap());
-    axum::serve(listener, app).await.unwrap();
+    axum::serve(
+        listener,
+        app.into_make_service_with_connect_info::<SocketAddr>(),
+    )
+    .await
+    .unwrap();
 }
