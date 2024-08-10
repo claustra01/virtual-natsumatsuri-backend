ab2534ce79bf53fa74b2c8d49b8ded60d5f9e3d5
claustra01 <shinya430.g@gmail.com>
2024-08-10 13:51:50 +0900

    add: shooter struct model

---
+++ src/model/shooter.rs
@@ -0,0 +1,28 @@
+#[derive(Debug, Deserialize)]
+struct angle {
+  pub x: f64,
+  pub y: f64,
+}
+
+#[derive(Debug, Deserialize)]
+struct acceleration {
+  pub x: f64,
+  pub y: f64,
+  pub z: f64,
+}
+
+#[derive(Debug, Deserialize)]
+struct distance {
+  pub x: f64,
+  pub y: f64,
+  pub z: f64,
+}
+
+#[derive(Debug, Deserialize)]
+struct shooter_data {
+  pub id: str,
+  pub angle: angle,
+  pub acceleration: acceleration,
+  pub distance: distance,
+  pub interval: f64,
+}
