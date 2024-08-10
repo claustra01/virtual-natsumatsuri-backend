4aa20fc95da7cb46cba74da4e4ae15292e06fe9a
claustra01 <shinya430.g@gmail.com>
2024-08-10 13:54:09 +0900

    fix: format

--- src/model/shooter.rs
+++ src/model/shooter.rs
@@ -2,29 +2,29 @@ use serde::Deserialize;
 
 #[derive(Debug, Deserialize)]
 struct angle {
-  pub x: f64,
-  pub y: f64,
+    pub x: f64,
+    pub y: f64,
 }
 
 #[derive(Debug, Deserialize)]
 struct acceleration {
-  pub x: f64,
-  pub y: f64,
-  pub z: f64,
+    pub x: f64,
+    pub y: f64,
+    pub z: f64,
 }
 
 #[derive(Debug, Deserialize)]
 struct distance {
-  pub x: f64,
-  pub y: f64,
-  pub z: f64,
+    pub x: f64,
+    pub y: f64,
+    pub z: f64,
 }
 
 #[derive(Debug, Deserialize)]
 struct shooter_data {
-  pub id: String,
-  pub angle: angle,
-  pub acceleration: acceleration,
-  pub distance: distance,
-  pub interval: f64,
+    pub id: String,
+    pub angle: angle,
+    pub acceleration: acceleration,
+    pub distance: distance,
+    pub interval: f64,
 }
